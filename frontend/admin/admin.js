89f7317dbbef6225caae6d2cb7a69c8e421ae0ea
Salman Bari <salmanbari914@gmail.com>
2026-07-30 15:47:03 +0500

    Complete Day 7 Morning - Website polish and legal pages

---
+++ frontend/admin/admin.js
@@ -0,0 +1,85 @@
+let allContacts = [];
+async function loadContacts() {
+
+
+    const table = document.getElementById("leadTable");
+
+    table.innerHTML = "";
+
+    try {
+
+        const response = await fetch("http://127.0.0.1:8000/api/contacts");
+
+        allContacts = await response.json();
+
+        renderTable(allContacts);
+
+        document.getElementById("totalLeads").textContent = allContacts.length;
+
+        const ai = allContacts.filter(
+            c => c.project.toLowerCase().includes("ai")
+        ).length;
+
+        const automation = allContacts.filter(
+            c => c.project.toLowerCase().includes("automation")
+        ).length;
+
+        document.getElementById("aiProjects").textContent = ai;
+
+        document.getElementById("automationProjects").textContent = automation;
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+    }
+
+}
+
+loadContacts();
+function renderTable(contacts){
+
+    const table = document.getElementById("leadTable");
+
+    table.innerHTML = "";
+
+    contacts.forEach(contact=>{
+
+        table.innerHTML += `
+
+        <tr>
+
+            <td>${contact.name}</td>
+
+            <td>${contact.company}</td>
+
+            <td>${contact.email}</td>
+
+            <td>${contact.project}</td>
+
+        </tr>
+
+        `;
+
+    });
+
+}
+document.getElementById("searchInput").addEventListener("input", function(){
+
+    const keyword = this.value.toLowerCase();
+
+    const filtered = allContacts.filter(contact =>
+
+        contact.name.toLowerCase().includes(keyword) ||
+
+        contact.company.toLowerCase().includes(keyword) ||
+
+        contact.email.toLowerCase().includes(keyword)
+
+    );
+
+    renderTable(filtered);
+
+});
---
+++ js/app.js
@@ -0,0 +1,53 @@
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", async function (e) {
+
+    e.preventDefault();
+
+    const data = {
+
+        name: document.getElementById("name").value,
+
+        company: document.getElementById("company").value,
+
+        email: document.getElementById("email").value,
+
+        project: document.getElementById("project").value,
+
+        message: document.getElementById("message").value
+
+    };
+
+    try {
+
+        const response = await fetch("http://127.0.0.1:8000/api/contact", {
+
+            method: "POST",
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            },
+
+            body: JSON.stringify(data)
+
+        });
+
+        const result = await response.json();
+
+        alert(result.message);
+
+        form.reset();
+
+    }
+
+    catch (error) {
+
+        alert("Server connection failed.");
+
+        console.log(error);
+
+    }
+
+});
